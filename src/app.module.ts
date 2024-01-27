ebae56ebf69f7985d353a40a8144a5026baf1995
TaeHyeon Woo <wth2052@gmail.com>
2024-01-27 23:09:03 +0900

    ♻REFACTOR : 모듈 의존성 변경
    - JWTModule Global화

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './module/users/users.module';
 import { AuthModule } from './module/auth/auth.module';
 import { typeormConfig } from './config/database/typeorm.config';
 import { TokenModule } from './module/token/token.module';
+import { BoardsModule } from './module/boards/boards.module';
+import { CommentsModule } from './module/comments/comments.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
@@ -15,8 +18,19 @@ import { TokenModule } from './module/token/token.module';
     AuthModule,
     UsersModule,
     TokenModule,
+    BoardsModule,
+    CommentsModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      global: true,
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('HEALING_AREA_JWT_SECRET_KEY'),
+      }),
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
+  exports: [JwtModule],
 })
 export class AppModule {}
--- src/module/users/users.module.ts
+++ src/module/users/users.module.ts
@@ -4,20 +4,8 @@ import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Users } from './entities/users.entity';
 import { UsersRepository } from './users.repository';
-import { JwtModule } from '@nestjs/jwt';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([Users]),
-    JwtModule.registerAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => ({
-        secret: configService.get<string>('HEALING_AREA_JWT_SECRET_KEY'),
-      }),
-    }),
-  ],
+  imports: [TypeOrmModule.forFeature([Users])],
   controllers: [UsersController],
   providers: [UsersService, UsersRepository],
   exports: [UsersService],
