cff6fdfe68ad540cd6cb980e90cb1470fbc9545c
TaeHyeon Woo <wth2052@gmail.com>
2024-01-27 23:18:05 +0900

    ✨FEAT : users-info.dto.ts, successmessage.enum.ts - 토큰 dto - 메세지 분기 enum 추가

--- src/common/enum/successmessage.enum.ts
+++ src/common/enum/successmessage.enum.ts
@@ -6,4 +6,6 @@ export enum SuccessMessage {
   USER_LOGOUT = `User ${CommonSuccessMessage.LOGOUT}`,
   REFRESH_TOKEN_GENERATED = `Refresh token ${CommonSuccessMessage.GENERATED}`,
   REFRESH_TOKEN_UPDATED = `Refresh token ${CommonSuccessMessage.UPDATED}`,
+  BOARD_CREATED = `Board ${CommonSuccessMessage.CREATED}`,
+  BOARD_UPDATED = `Board ${CommonSuccessMessage.UPDATED}`,
 }
