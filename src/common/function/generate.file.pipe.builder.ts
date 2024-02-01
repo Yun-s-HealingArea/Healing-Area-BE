81ba07e4b4d387830bc42f526e6139c8ddcb6823
TaeHyeon Woo <wth2052@gmail.com>
2024-02-02 04:12:03 +0900

    🚑HOTFIX : validation pipe
    - 최대 크기 10메가

--- src/common/function/generate.file.pipe.builder.ts
+++ src/common/function/generate.file.pipe.builder.ts
@@ -4,20 +4,14 @@ import { ErrorMessage } from '../enum/errormessage.enum';
 export function generateFilePipeBuilder() {
   return new ParseFilePipeBuilder()
     .addFileTypeValidator({
-      fileType: 'png',
-    })
-    .addFileTypeValidator({
-      fileType: 'jpeg',
+      fileType: /(jpg|jpeg|png|gif)$/,
     })
     .addMaxSizeValidator({
       maxSize: 10 * 1024 * 1024, // just to you know it's possible.
     })
     .build({
       exceptionFactory() {
-        throw new BadRequestException([
-          ErrorMessage.FILE_TO_LARGE,
-          ErrorMessage.INVALID_FILE_TYPE,
-        ]);
+        throw new BadRequestException([ErrorMessage.FILE_TO_LARGE]);
       },
     });
 }
