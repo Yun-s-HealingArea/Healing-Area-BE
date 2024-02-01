230df06c6f6b38a79bda74a7cb1ce23e0e0ca482
TaeHyeon Woo <wth2052@gmail.com>
2024-02-02 00:01:08 +0900

    ✨FEAT : likes Validation
    - 중복 좋아요 금지
    - 같은 게시글 좋아요시 400

--- src/module/likes/likes.service.ts
+++ src/module/likes/likes.service.ts
@@ -1,9 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateLikeDTO } from './dto/create-like.dto';
 import { LikesRepository } from './likes.repository';
 import { UsersService } from '../users/users.service';
 import { BoardsService } from '../boards/boards.service';
 import { UpdateLikeDTO } from './dto/update-like.dto';
+import { ErrorMessage } from '../../common/enum/errormessage.enum';
+import { generateMessageObject } from '../../common/function/generate.message.object.function';
+import { SuccessMessage } from '../../common/enum/successmessage.enum';
 
 @Injectable()
 export class LikesService {
@@ -13,17 +16,30 @@ export class LikesService {
     private readonly boardsService: BoardsService,
   ) {}
   async create(createLikeDTO: CreateLikeDTO) {
-    console.log(createLikeDTO);
-    const users = await this.usersService.findOne(createLikeDTO.usersId);
+    // const users = await this.usersService.findOne(createLikeDTO.usersId);
+    const users = await this.usersService.findOneByEmail(
+      createLikeDTO.usersEmail,
+    );
     const boards = await this.boardsService.findOne(createLikeDTO.boardsId);
-    console.log(users, boards);
-
-    const createdLikes = this.likesRepository.create({
-      users,
-      boards,
+    const isUserAlreadyLiked = await this.likesRepository.exist({
+      where: {
+        users: { id: users.id },
+        boards: { id: boards.id },
+      },
     });
-    await this.likesRepository.save(createdLikes);
-    return 'This action adds a new like';
+    //TODO: TypeScript에서는 throw 문이 표현식이 아니기 때문에 삼항 연산자의 일부로 직접 사용할 수 없음
+    // if문인데 좀 더 깔끔하게 분기할 수 있는 방법이 없을까? (if.. 극..혐..)
+    if (isUserAlreadyLiked) {
+      throw new BadRequestException([ErrorMessage.LIKE_ALREADY_EXIST]);
+    } else {
+      const createdLikes = this.likesRepository.create({
+        users,
+        boards,
+      });
+      await this.likesRepository.save(createdLikes);
+    }
+
+    return generateMessageObject(SuccessMessage.LIKE_CREATED);
   }
 
   findAll() {
